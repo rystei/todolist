4279e670e8b9fdf3cc7b7a79c63544ae0103a961
rystei <gustavo.telles@edu.unifil.br>
2025-07-25 12:52:55 -0300

    Realizando o filtro

--- src/main/java/br/com/rystei/todolist/task/TaskController.java
+++ src/main/java/br/com/rystei/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ public class TaskController {
 
     @PostMapping("/")
     public TaskModel create(@RequestBody TaskModel taskModel) {
+        System.out.println("chegou no controller");
         var task = this.taskRepository.save(taskModel);
         return task;
     }
