dc63f2cdb6c0bad86c556e634602c23ac1f3aac0
rystei <gustavo.telles@edu.unifil.br>
2025-07-25 10:33:45 -0300

    Hash de senha

--- src/main/java/br/com/rystei/todolist/user/UserCrontroller.java
+++ src/main/java/br/com/rystei/todolist/user/UserCrontroller.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserCrontroller {
@@ -24,6 +26,10 @@ public class UserCrontroller {
             System.out.println("Usuário já existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
