63ea92df1a74c8018991ff8dc56b2d7ff1260eb0
rystei <gustavo.telles@edu.unifil.br>
2025-07-25 13:25:40 -0300

    implementações no filtro

--- src/main/java/br/com/rystei/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rystei/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,42 @@
 package br.com.rystei.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter{
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+                
+                var authorization = request.getHeader("authorization");
+
+                var authEncoded = authorization.substring("Bacic".length()).trim();
+
+                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecode);
+
+                String[] creadentials = authString.split(":");
+                String username = creadentials[0];
+                String password = creadentials[1];
+                System.out.println("Authorization");
+                System.out.println(username);
+                System.out.println(password);
+                
+
+                filterChain.doFilter(request, response);
     }
+
+
     
 }
